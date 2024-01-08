50f94e2abe0937f78b8a011cd1a609c07d260d80
Huang Jihao <705626108@qq.com>
2024-01-08 20:27:59 +0800

    fix:/help功能从发送文字变成发送图片

--- command/help.py
+++ command/help.py
@@ -11,7 +11,7 @@ def get_help_msg() -> str:
         cmd_msg = ""
         for key in value["keys"]:
             cmd_msg += "/" + key + "\n"
-        help_msg += cmd_msg + "➡「" + value["desc"] + "」\n"
+        help_msg += cmd_msg + "-->「" + value["desc"] + "」\n\n"
     return help_msg
 
 # def get_help_image() -> str:
